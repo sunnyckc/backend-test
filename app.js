a4cc6e5ef93e05b22ccabadf3fb98695a8458575
sunnyckc <sunnychu@mojocore.com>
2023-03-10 22:07:38 +0800

    added the let's encrypt file

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-import express from 'express';
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
+const options = {
+  key: fs.readFileSync('/path/to/privkey.pem'),
+  cert: fs.readFileSync('/path/to/fullchain.pem')
+};
+
+https.createServer(options, app).listen(443);
+
 app.get('/', (req, res) => {
-    res.send('Hello World! This is version 1');
+  res.send('Hello, world!');
 });
 
 app.listen( 80 , () => {
