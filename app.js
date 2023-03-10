0eb32b42283bab7ba480d1212bc557ee3ea6c3b9
sunnyckc <sunnychu@mojocore.com>
2023-03-10 16:11:13 +0800

    this is the first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World! This is version 1');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
